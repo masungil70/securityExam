2111776b5d12e51028b27c7cb56c0f4ae9082141
masun <masungil70@gmail.com>
2023-05-10 13:11:39 +0900

    프로젝트 최초 생성

---
+++ src/main/java/kr/or/kosa/SecurityExamApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityExamApplication.class, args);
+	}
+
+}
